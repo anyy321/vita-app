12751af3996f40f24d1364f1205c2d8876f71f2e
anyy321 <Massagemsaqua@gmail.com>
2026-08-19 23:39:35 -0300

    Correct content paths in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content:content: ["./index.html", "./*.{js,jsx}"],
   theme: {
     extend: {},
   },
